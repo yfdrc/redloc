2541ee06c2083fcd65601b5ee178c185154dbfc4
yfdrc <gdyfqxdrc@qq.com>
2018-11-02 16:30:20 +0800

    fix tools 06

--- app/src/main/java/com/drc/tools/Crypto/TestCrypto.java
+++ app/src/main/java/com/drc/tools/Crypto/TestCrypto.java
@@ -2,40 +2,36 @@ package com.drc.tools.Crypto;
 
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestCrypto {
-    public static final void Test(){
+    public static final void Test() {
         int testMaxCount = 1;//测试的最大数据条数
         //添加测试数据
         for (int i = 0; i < testMaxCount; i++) {
             //生成一个动态key
-            String secretKey = AesUtils.generateKey();
-            byte[] rawKey = null;
-            try {
-                rawKey = AesUtils.getRawKey(secretKey);
-            }catch (Exception ex){
-            }
-            Log.e("drc", "动态Key -> " + secretKey);
-            Log.e("drc", "Keyraw -> " + rawKey.toString());
-            Log.e("drc", "salt -> " + AesUtils.getSalt(32).toString());
+            String key = "red01";
+            Log.i("drc", "Key  -> " + key);
 
-            //FastJson生成json数据
-            String jsonData = String.format("No:%s ;Name:%s",i,i)+":";
-            Log.e("drc", "加密前 -> " + jsonData);
+            byte[] salt = AesUtils.generateSalt(32);
 
-            //AES加密
-            long start = System.currentTimeMillis();
-            String encryStr = AesUtils.encrypt(rawKey, jsonData);
-            long end = System.currentTimeMillis();
-            Log.e("drc", "加密后 -> " + encryStr);
+            byte[] raw = AesUtils.getRawKey(key, salt);
 
-            //AES解密
-            start = System.currentTimeMillis();
-            String decryStr = AesUtils.decrypt(rawKey, encryStr);
-            end = System.currentTimeMillis();
-            Log.e("drc", "解密后 -> " + decryStr);
+            String jsonData = String.format("No:%s ;Name:%s", i, i) + ":";
+            Log.w("drc", "加密前 -> " + jsonData);
+
+            String encryStr = AesUtils.encrypt(raw, jsonData);
+            Log.v("drc", "加密后 -> " + encryStr);
+
+            String saltstr = AesUtils.SaveSalt(salt);
+            byte[] salt2 = AesUtils.ReadSalt(saltstr);
+            Log.i("drc", "salt -> " + AesUtils.toHexString(salt));
+            Log.i("drc", "salt2-> " + AesUtils.toHexString(salt2));
+            byte[] raw2 = AesUtils.getRawKey(key, salt2);
+            Log.i("drc", "raw  -> " + AesUtils.toHexString(raw));
+            Log.v("drc", "raw2 -> " + AesUtils.toHexString(raw2));
+
+            String decryStr = AesUtils.decrypt(raw2, encryStr);
+            Log.w("drc", "解密后 -> " + decryStr);
+            Log.w("drc", " " );
         }
     }
 }
